2765d342dedc875da42ff226dc0f4f91b9542e73
Dani Scarpa <d.scarpa@bboxx.co.uk>
2019-06-12 21:19:45 +0100

    Changed method isUnique to accept unicode and tests added.
    The method isUnique is now using a integer (32 bit) as a checker
    and it is put in end with a 1 shifted val times, where val is the
    value of the char. If && is positive means that the same value was
    added to the checker previously. To update the checker, the new 1 shifted
    val times is added with an or (|) operation.
    This new version of is Unique is able to check any character.

--- cracking_the_coding_interview/chapter01/Exercise1_1.py
+++ cracking_the_coding_interview/chapter01/Exercise1_1.py
@@ -1,17 +1,38 @@
 # -*- coding: utf-8 -*-
+import unittest
+
+def getVal(ch):
+    return ord(ch)
 
 def isUnique(s):
     """
     Implement an algorithm to determine if a string has all unique characters.
-    What if you cannot use additional data structures?
+    This method is able to check unicode strings.
     """
-    visited = {}
+    # First I create a integer (32 bit) zero (32 zero bits).
+    checker = 0
     for ch in s:
-        if not visited.get(ch):
-            visited[ch] = True
-        else:
+        # for each character i shift the 1 as many times as the value of
+        # the char, calculated as for the getVal method.
+        check = (1 << getVal(ch))
+        # if checker and check > 0 means that that the one has been
+        # shfted already the same positions, that means that the 
+        # same letter has been found previously. In this case the 
+        # string is not unique (return False).
+        if (checker & check) > 0:
             return False
-    return True
+        # I update the checker with the new 1 in the position check 
+        # (found using getVal).
+        checker = checker | check
+    return True 
+
+
+class isUniqueTest(unittest.TestCase):
+    def testIsUnique(self):
+        self.assertEqual(isUnique('thisisfalse'), False)
+        self.assertEqual(isUnique('thisłopn'), True)
+        self.assertEqual(isUnique('1jehwkua764392'), True)
+        self.assertEqual(isUnique('1jehwkua76439211'), False)
 
 if __name__ == '__main__':
-    print isUnique('łlo')
+    unittest.main()
